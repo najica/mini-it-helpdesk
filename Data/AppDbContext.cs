b39040f91e6a4d7cdf814659c3930e4b321154bf
Luka-Vukovic <lukav400@gmail.com>
2026-08-17 11:43:58 +0200

    Save my progress

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MiniItHelpdesk.Models;
 
 namespace MiniItHelpdesk.Data
 {
@@ -7,6 +8,29 @@ namespace MiniItHelpdesk.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
         {
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<User>().Property(u => u.Name);
+            modelBuilder.Entity<User>().Property(u => u.Email);
+            modelBuilder.Entity<User>().Property(u => u.Role)
+                .HasConversion<string>()
+                .HasColumnType("TEXT");
+            modelBuilder.Entity<Ticket>().HasKey(t => t.Id);
+            modelBuilder.Entity<Ticket>().Property(t => t.Status)
+                .HasConversion<string>()
+                .HasColumnType("TEXT");
+            modelBuilder.Entity<Ticket>().Property(t => t.Priority)
+                .HasConversion<string>()
+                .HasColumnType("TEXT");
+
         }
     }
 }
