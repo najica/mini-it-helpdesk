68ee68a5592a2992fc9e13aa8136ae18f31dbe3b
lukaajnstajn <luka.ajnstajn@gmail.com>
2026-08-17 10:07:38 +0200

    Define User Model

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniItHelpdesk.Models
+{
+    public class User
+    {
+        public int Id;
+        public string Name;
+        public string Email;
+        public enum UserRole
+        {
+            Employee, ITAgent, Admin, 
+        }
+
+        public UserRole Role;
+    }
+}
