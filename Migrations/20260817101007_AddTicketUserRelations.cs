f736edad6d3df5e4ee1302133d25e35127725b6f
Luka-Vukovic <164051385+Luka-Vukovic@users.noreply.github.com>
2026-08-17 12:17:37 +0200

    Add user-ticket relations

---
+++ Migrations/20260817101007_AddTicketUserRelations.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniItHelpdesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTicketUserRelations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AssignedToUserId",
+                table: "Tickets",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedByUserId",
+                table: "Tickets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_AssignedToUserId",
+                table: "Tickets",
+                column: "AssignedToUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_CreatedByUserId",
+                table: "Tickets",
+                column: "CreatedByUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Users_AssignedToUserId",
+                table: "Tickets",
+                column: "AssignedToUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Users_CreatedByUserId",
+                table: "Tickets",
+                column: "CreatedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Users_AssignedToUserId",
+                table: "Tickets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Users_CreatedByUserId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_AssignedToUserId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_CreatedByUserId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "AssignedToUserId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Tickets");
+        }
+    }
+}
