388a450521df8c4bcb7ea7a2ee3d8d7c2a26236c
Luka-Vukovic <164051385+Luka-Vukovic@users.noreply.github.com>
2026-08-24 11:33:30 +0200

    Add user model and service

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly apiUrl = `${environment.apiUrl}/users`;
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+}
