070bbb193ba01d8b44520236f9f07429a9ee2713
Luka-Vukovic <164051385+Luka-Vukovic@users.noreply.github.com>
2026-08-24 12:10:34 +0200

    Add tickets search on frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { TicketListComponent } from './ticket-list/ticket-list.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [
